ea2bce00a24594e1c571aa057320dae6215ed460
Thái Sơn Hoàng <T.S.Hoang@ecs.soton.ac.uk>
2018-05-08 09:18:19 +0100

    VHDL Generator test: Added VXMI Translator Utils Tests.

---
+++ ac.soton.coda.vhdl.generator.tests/src/ac/soton/coda/internal/vxmiTranslator/tests/package-info.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * (c) Crown owned copyright (2018) (UK Ministry of Defence)
+ *
+ * All rights reserved. This program and the accompanying materials are 
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      University of Southampton - Initial API and implementation
+ *******************************************************************************/
+/**
+ * This package contains tests for {@link ac.soton.coda.internal.vxmi2vhdl} package
+ * 
+ * @author htson
+ * @version 0.1
+ * @see ac.soton.coda.vhdl.vxmiTranslator
+ * @since 0.0.2
+ */
+package ac.soton.coda.internal.vxmiTranslator.tests;
